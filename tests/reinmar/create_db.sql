f57cad0f006a9a06c34d562970431bb0afe3d479
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2011-10-13 18:34:38 +0200

    fixed importer

--- tests/reinmar/create_db.sql
+++ tests/reinmar/create_db.sql
@@ -3,3 +3,14 @@ CREATE CLASS ASConn EXTENDS OGraphEdge;
 CREATE PROPERTY ASNode.name string;
 CREATE PROPERTY ASNode.num integer;
 CREATE PROPERTY ASConn.up boolean;
+
+CREATE DATABASE remote:localhost/asvis root root local
+CREATE CLASS ASNode;
+CREATE CLASS ASConn;
+CREATE PROPERTY ASNode.in linkset ASConn;
+CREATE PROPERTY ASNode.out linkset ASConn;
+CREATE PROPERTY ASNode.name string;
+CREATE PROPERTY ASNode.num integer;
+CREATE PROPERTY ASConn.in link ASNode;
+CREATE PROPERTY ASConn.out link ASNode;
+CREATE PROPERTY ASConn.up boolean;
