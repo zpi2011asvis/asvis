410ad3a2272dd7ebcd7c177a7170de1685531c3f
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2011-10-18 23:45:40 +0200

    taby

--- backend/lib/Response.php
+++ backend/lib/Response.php
@@ -4,7 +4,7 @@ namespace asvis\lib;
 use \Response as TonicResponse;
 
 class Response extends TonicResponse {
-    function __construct($request, $uri = NULL) {
+	function __construct($request, $uri = NULL) {
 		parent::__construct($request, $uri);
 	}
 
