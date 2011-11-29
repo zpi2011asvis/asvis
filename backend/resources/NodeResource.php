56746ec29af19c2a6f6c8686c842b42df08c9f8a
mactoob <mactoo@gmail.com>
2011-11-29 19:09:26 +0100

    Poprawka w node resource - mysqlEngine

--- backend/resources/NodeResource.php
+++ backend/resources/NodeResource.php
@@ -18,8 +18,8 @@ class NodesFindResource extends Resource {
 		$response->s404Unless($number, 'Nie przekazano prawidłowego numeru AS.');
 		
 		if ($response->code === 200) {
-			$this->_engine = new MySQLEngine();
-			$forJSON = $this->_engine->nodesFind($number);
+			$mysqlEngine = new MySQLEngine();
+			$forJSON = $mysqlEngine->nodesFind($number);
 			$response->s404If(empty($forJSON), 'Nie istnieje AS o podanym numerze.');
 			$response->s500If(is_null($forJSON), 'Błąd pobierania danych z bazy.');
 		
@@ -41,8 +41,8 @@ class NodesMetaResource extends Resource {
 		$response->s404If(empty($numbers), 'Nie przekazano żadnych numerów AS.');
 
 		if ($response->code === 200) {
-			$this->_engine = new MySQLEngine();
-			$forJSON = $this->_engine->nodesMeta($numbers);
+			$mysqlEngine = new MySQLEngine();
+			$forJSON = $mysqlEngine->nodesMeta($numbers);
 			$response->s404If(empty($forJSON), 'Nie znaleziono informacji na temat wszystkich AS o przekazanych numerach.');
 			$response->s500If(is_null($forJSON), 'Błąd pobierania danych z bazy.');
 		
