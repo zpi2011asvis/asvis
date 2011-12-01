e296c4cb843e19a493e7a4451f9cc115554934fb
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2011-12-01 03:43:30 +0100

    autocompleter 100%

--- scripts/insert_ases.sql
+++ scripts/insert_ases.sql
@@ -10,3 +10,11 @@ SELECT DISTINCT(ASNum), CONCAT('AS', ASNum) FROM (
 	UNION
 	SELECT asnumup AS asnum FROM asup
 ) AS asnums WHERE asnum > -1;
+
+DELETE FROM ases WHERE asnum NOT IN (
+SELECT DISTINCT(asnum) as asnum FROM (
+	SELECT asnum FROM asup WHERE asnumup <> -1 UNION
+	SELECT asnum FROM asdown WHERE asnumdown <> -1 UNION
+	SELECT asnumup AS asnum FROM asup WHERE asnumup <> -1 UNION
+	SELECT asnumdown AS asnum FROM asdown WHERE asnumdown <> -1
+) AS asnums); 
