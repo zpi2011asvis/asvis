20668374a353868bec3b067105ba4a6c0106f826
mactoob <mactoo@gmail.com>
2011-12-12 10:05:33 +0100

    NodeDBEngine - structureTree()

---
+++ backend/lib/nodedb/Node.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace asvis\lib\nodedb;
+
+class Node {
+	
+	/**
+	 * AS number
+	 * @var int
+	 */
+	private $num;
+	
+	/**
+	 * Outgoing connections
+	 * @var array of int
+	 */
+	public $out;
+	
+	/**
+	 * Incoming connections
+	 * @var array of int
+	 */
+	public $in;
+	
+	/**
+	 * Distance from root node
+	 * @var int
+	 */
+	public $distance;
+	
+	/**
+	 * Node weight (sum of incoming and outgoing connections count)
+	 * @var int
+	 */
+	public $weight;
+	
+	/**
+	 * @param string $rid
+	 * @param int $num
+	 * @param string $name
+	 * @param array of int $out
+	 * @param array of int $in
+	 * @param int $distance
+	 */
+	public function __construct($num = null, $out = null, $in = null, $distance = null, $weight = null) {
+		$this->num	= $num;
+		
+		$this->out	= $out;
+		$this->in	= $in;
+		
+		$this->distance	= $distance;
+		$this->weight = $weight;
+	}
+	
+	public function __get($name) {
+		if(isset($this->$name)) {
+			return $this->$name;
+		} else {
+			return null;
+		}
+	}
+	
+	public function __set($name, $value) {
+		if(isset($this->$name)) {
+			$this->$name = $value;
+		}
+	}
+	
+}
---
+++ backend/lib/nodedb/Structure.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace asvis\lib\orient;
+
+require_once 'Node.php';
+
+use asvis\lib\nodedb\Node as Node;
+
+/**
+ * Storage class for ASNodes
+ */
+class Structure {
+	
+	/**
+	 * Structure
+	 * @var array of ASNode
+	 */
+	protected $_structure;
+	protected $_weight_order;
+	protected $_distance_order;
+	
+	/**
+	 * @param array of mixed $structure output of Engine::structureGraph or Engine::structureTree
+	 */
+	public function __construct($structure = null, $weight_order = null, $distance_order = null) {
+		if ($structure == null) {
+			$this->_structure = array();
+		} else {
+			foreach ($structure as $num => $node) {
+				$out = array_values($node->out);				
+				$in = array_values($node->in);
+							
+				$this->_structure[$num] =
+					new Node(
+						$num,
+						$out,
+						$in,
+						$node->distance,
+						$node->weight
+					);
+			}
+		}
+		
+		if ($weight_order == null) {
+			$this->_weight_order = array();
+		} else {
+			$this->_weight_order = $weight_order;
+		}
+		
+		if ($distance_order == null) {
+			$this->_distance_order = array();
+		} else {
+			$this->_distance_order = $distance_order;
+		}
+	}
+	
+	/**
+	 * Return ASNode with given num;
+	 * @param int $num
+	 * @return ASNode
+	 */
+	public function get($num) {
+		return $this->_structure[$num];
+	}
+	
+	/**
+	 * Return structure in JSON-ready form
+	 * @return array of ASNode
+	 */
+	public function forJSON() {
+		$toEncode = array();
+		
+		$toEncode['structure'] = $this->_structure;
+		$toEncode['weight_order']	= $this->_weight_order;
+		$toEncode['distance_order']	= $this->_distance_order;
+		
+		return $toEncode;
+	}
+	
+}
